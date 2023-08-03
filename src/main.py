2f2a2d9f67d6080440d03a4a91a7e6a36b854b08
Thomas van Erven <thomas.van.erven@dans.knaw.nl>
2023-08-03 11:22:13 +0200

    Update.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from json import JSONDecodeError
-from fastapi import FastAPI, Request, Response
-from client import GettyClient
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 
 app = FastAPI()
+
+origins = [
+    "http://ohsmart.dansdemo.nl",
+    "https://ohsmart.dansdemo.nl",
+    "http://localhost:5489",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ProxyResponse(BaseModel):
 
@@ -15,10 +29,18 @@ class ProxyResponse(BaseModel):
 
 @app.get("/proxy")
 def proxy(url: str):
-    response = requests.get(
-        verify=False,
-        url=url
-    )
+    try:
+        response = requests.get(
+            verify=False,
+            url=url
+        )
+    except ConnectionError:
+        return ProxyResponse(
+            status_code=418,
+            text='URL does not actually resolve, verify that its correct',
+            request_url=url,
+            json_response=None
+        )
     try:
         json = response.json()
     except JSONDecodeError:
